faee8c03a3914e5022edda11f6593b6607da5e37
Philip <Cheesewizard06@hotmail.com>
2021-07-14 14:20:22 +0100

    Unit 2 Challenge

---
+++ Challenge_2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float keyDelay = 2.0f;
+
+    private float previousTime = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Delays pressing the space key again for x seconds. 
+            var time = Time.time;
+            var deltaTime = time - previousTime;
+
+            if(deltaTime > keyDelay)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                previousTime = time;
+            }
+        }
+    }
+}
--- Junior_Programmer/Assets/Scripts/Unit 2/DestroyOutOfBounds.cs
+++ Junior_Programmer/Assets/Scripts/Unit 2/DestroyOutOfBounds.cs
@@ -10,18 +10,20 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.z > topBound)
+        // Removes objects that are out of bounds
+        if (transform.position.z > topBound)
         {
             Destroy(gameObject);
         }
         else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over");
             Destroy(gameObject);
         }
     }
